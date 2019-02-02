772f8880edd309307d8e5b41500ec56abdd74705
Caitlin Ying <caitlinying@Caitlins-MacBook-Pro.local>
2019-02-02 09:10:03 -0500

    All the other stuff that I did before sleep

---
+++ AndroidStudioProjects/Rosebud/app/src/main/java/com/example/rosebud/LoginScreen.java
@@ -0,0 +1,15 @@
+package com.example.rosebud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginScreen extends AppCompatActivity {
+
+    private
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
